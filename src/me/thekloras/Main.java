8f7dbaba0a80cb18d0a29b18715497e30807852c
Moksleivis <@>
2021-12-23 10:15:02 +0200

    +

--- src/me/thekloras/Main.java
+++ src/me/thekloras/Main.java
@@ -6,33 +6,46 @@ public class Main {
 
     public static void main(String[] args) {
         LinkedList sarasasList = new LinkedList();
-        sarasasList.add(256);
-        sarasasList.add(541);
-        sarasasList.add(851);
-        sarasasList.add(965);
-        sarasasList.add(256);
-        sarasasList.add(965);
-        sarasasList.add(252);
-        sarasasList.add(7788);
-        sarasasList.add(0);
-        sarasasList.add(77);
+        sarasasList.add(1);
+        sarasasList.add(1);
+        sarasasList.add(2);
+        sarasasList.add(2);
+        ///sarasasList.add(7474);
+        //sarasasList.add(0);
+        //sarasasList.add(0);
+        //sarasasList.add(-8585);
+        //sarasasList.add(0);
+        //sarasasList.add(0);
 
         spausdinti(sarasasList);
 
-        for(int i = 0;sarasasList.size()>i;i++){
-            for(int k = 0;sarasasList.size()>i;i++){
-                if(sarasasList.get(i)==sarasasList.get(k)){
-                    sarasasList.remove(k);
+        //kodas duplikatams iš LinkedList panaikinimui
+        for (int i = 0; sarasasList.size() > i; i++) {
+            for (int k = 0; sarasasList.size() > k; k++) {
+                try {
+                    if (sarasasList.get(i).equals(sarasasList.get(k)) && i != k) {
+                        System.out.println("Panaikinamas " + k + "-asis elementas iš LinkedList kurio reikšmė yra " + sarasasList.get(k));
+                        sarasasList.remove(k);
+                    }
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println(e);
+                } catch (Exception e) {
+                    System.out.println(e);
                 }
+                //System.out.println(i + " " + k);
             }
         }
+
         spausdinti(sarasasList);
     }
-    public static void spausdinti(LinkedList sarasasList){
-        for(int i = 0;sarasasList.size()>i;i++){
+
+    //funkcija LinkedList spausdinimui
+    public static void spausdinti(LinkedList sarasasList) {
+        System.out.println("--------------------------");
+        for (int i = 0; sarasasList.size() > i; i++) {
             System.out.print(sarasasList.get(i) + " ");
         }
-        System.out.println("");
+        System.out.println();
         System.out.println("--------------------------");
     }
 }
